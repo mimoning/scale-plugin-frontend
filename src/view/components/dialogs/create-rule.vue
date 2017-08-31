133eb0db07cbf17e76e22d3b9134028a5dc84563
MimoN <mimo.ouyang@daocloud.io>
2017-08-31 22:22:40 +0800

    fix(create-rule): 修改创建规则的部分逻辑

--- src/view/components/dialogs/create-rule.vue
+++ src/view/components/dialogs/create-rule.vue
@@ -37,12 +37,12 @@
           <dao-setting-item>
             <template slot="content">
               <div>
-                如果以下条件 <dao-select class="condition-select" v-model="condition" size="sm">
+                如果以下条件 <dao-select class="condition-select" v-model="dataConfig.condition.logic" size="sm">
                   <dao-option :value="1">全部</dao-option>
                   <dao-option :value="0">任意</dao-option>
                 </dao-select> 满足，则触发扩容
               </div>
-              <dao-editable-table v-model="dataConfig.triggerCondition"
+              <dao-editable-table v-model="triggerCondition"
                 :config="conditionConfig">
               </dao-editable-table>
               <div class="error-text" v-show="conditionError">
@@ -56,19 +56,19 @@
           <dao-setting-item>
             <template slot="label">单次扩容数量</template>
             <template slot="content">
-              <input type="number" v-model="dataConfig.step" class="dao-control">
+              <input type="number" v-model.number="dataConfig.step" class="dao-control">
             </template>
           </dao-setting-item>
           <dao-setting-item>
             <template slot="label">扫描间隔（s）</template>
             <template slot="content">
-              <input type="number" v-model="dataConfig.scan" class="dao-control">
+              <input type="number" v-model.number="dataConfig.scan" class="dao-control">
             </template>
           </dao-setting-item>
           <dao-setting-item>
             <template slot="label">扩容上限</template>
             <template slot="content">
-              <input type="number" v-model="dataConfig.limit" class="dao-control">
+              <input type="number" v-model.number="dataConfig.limit" class="dao-control">
             </template>
           </dao-setting-item>
         </dao-setting-section>
@@ -95,7 +95,9 @@ function _dataConfig () {
     // 数据
     name: '',
     service: '',
-    triggerCondition: [],
+    condition: {
+      logic: 1
+    },
     // step
     step: undefined,
     scan: undefined,
@@ -120,7 +122,6 @@ export default {
       // 侧边栏
       tab: 'basicInfo',
       // 触发条件
-      condition: 1,
       conditionConfig: {
         header: [
           '资源',
@@ -130,15 +131,15 @@ export default {
         body: [{
           type: 'select',
           name: 'name',
-          default: 'CPU',
+          default: 'cpu',
           options: [
             {
               label: 'CPU',
-              value: 'CPU'
+              value: 'cpu'
             },
             {
               label: '内存',
-              value: 'Memory'
+              value: 'mem'
             }
           ]
         }, {
@@ -147,31 +148,42 @@ export default {
           default: '大于'
         }, {
           type: 'input',
-          name: 'value'
+          name: 'value',
+          validate (data) {
+            if (!/^\d+$/.test(data.value)) {
+              return '阈值只能为数值'
+            }
+            if (data.value > 100) {
+              return '阈值不应该超过 100%'
+            }
+            if (data.value <= 0) {
+              return '阈值不应该小于或者等于 0'
+            }
+            return true
+          }
         }]
       },
+      conditionError: '',
+      triggerCondition: [],
       dataConfig: _dataConfig()
     }
   },
   computed: {
-    conditionError () {
-      // 如果去重后长度有改变，则说明有重复，则报错
-      if (_.uniqBy(this.dataConfig.triggerCondition, 'name').length < this.dataConfig.triggerCondition.length) {
-        return '不要重复对资源进行条件设置'
-      }
-      return ''
-    },
     // 表单完整度验证
     formComplete () {
-      return this.dataConfig.name && this.dataConfig.service && !this.conditionError &&
-        this.dataConfig.triggerCondition.length && this.dataConfig.step && this.dataConfig.scan &&
+      return this.dataConfig.name &&
+        this.dataConfig.service &&
+        (this.dataConfig.condition.cpu ||
+        this.dataConfig.condition.mem) &&
+        this.dataConfig.step &&
+        this.dataConfig.scan &&
         this.dataConfig.limit
     }
   },
   created () {
     // 订阅创建规则数据流
     this.create$$
-      .map(({ data }) => this.createParams(data))
+      .map(({ data }) => data)
       .concatMap(ruleApi.addRule)
       .subscribe(() => {
         hub$$.next('rule')
@@ -187,25 +199,32 @@ export default {
         this.tab = 'basicInfo'
         this.dataConfig = _dataConfig()
       }
+    },
+    // 监听触发条件数据变化，实时将数组转化成需要的键值对
+    triggerCondition (val) {
+      // 当数据出错时，产生错误信息
+      this.conditionError = this.isErrorCondition(val)
+      // 如果不出错时，将数据转化成正确格式的数据
+      if (!this.conditionError) {
+        // 这里需要将原有的 logic 数据复制过来
+        const condition = {
+          logic: this.dataConfig.condition.logic
+        }
+        // 遍历获取的触发条件数据，格式化成需要的键值对格式 { logic, cpu, mem }
+        _.forEach(val, item => {
+          condition[item.name] = window.parseInt(item.value)
+        })
+        this.dataConfig.condition = condition
+      }
     }
   },
   methods: {
-    createParams (data) {
-      const condition = _.reduce(data.triggerCondition, (sum, item) => {
-        if (sum) {
-          return `${sum} ${this.condition ? 'and' : 'or'} ${item.name}: ${item.value}%`
-        }
-        return `${sum}${item.name}: ${item.value}%`
-      }, '')
-
-      return {
-        name: data.name,
-        service: data.service,
-        condition,
-        scan: data.scan,
-        step: data.step,
-        limit: data.limit
+    isErrorCondition (val) {
+      // 如果去重后长度有改变，则说明有重复，则报错
+      if (_.uniqBy(val, 'name').length < val.length) {
+        return '不要重复对资源进行条件设置'
       }
+      return ''
     }
   }
 }
